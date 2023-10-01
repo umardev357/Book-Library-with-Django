af9533b1301f8e44f07abec1f9f23cd467f9cfbe
UmarDev <faroukabdallah357@gmail.com>
2023-10-01 14:46:30 +0000

    Update settings.py on the csrf forward slash test

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['web-production-6323.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-6323.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-6323.up.railway.app/']
 
 # Application definition
 
